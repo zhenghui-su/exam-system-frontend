50e904e464a75e0e4389d4a9c8b6dcaca44d38cf
zhenghui-su <343196323@qq.com>
2025-06-26 11:13:03 +0800

    feat(auth): 实现登录、注册
    - 新增 Login、Register 和 UpdatePassword 页面组件
    - 实现用户登录、注册和发送验证码功能
    - 添加全局路由配置
    - 移除旧的 App 组件和相关样式

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Register } from './pages/Register';
+import { Login } from './pages/Login';
+import { UpdatePassword } from './pages/UpdatePassword';
+import '@ant-design/v5-patch-for-react-19';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const routes = [
+	{
+		path: '/',
+		element: <div>index</div>,
+	},
+	{
+		path: 'login',
+		element: <Login />,
+	},
+	{
+		path: 'register',
+		element: <Register />,
+	},
+	{
+		path: 'update_password',
+		element: <UpdatePassword />,
+	},
+];
+const router = createBrowserRouter(routes);
+
+const root = ReactDOM.createRoot(
+	document.getElementById('root') as HTMLElement
+);
+
+root.render(<RouterProvider router={router} />);
